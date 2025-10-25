536a8f593801576c31ab56900145cc96a09a988f
Ahtesham Abdul Aziz <ahtesham2000@gmail.com>
2025-10-25 10:41:12 +0530

    update

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -733,7 +733,7 @@ class AdminController extends Controller
     public function testimonialList()
     {
 
-        $testimonials = TestimonialsModel::all();
+        $testimonials = TestimonialsModel::orderByDesc('id')->get();
 
         return view('administrator.dashboard.testimonials', compact('testimonials'));
     }
